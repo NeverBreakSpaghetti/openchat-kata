8a13c3aecc6fbd7fa5adf3d8d3d26e4b351db691
Gianni Bombelli <gbombelli@vitesicure.it>
2024-03-15 15:39:34 +0100

    first commit
    Signed-off-by: Gianni Bombelli <gbombelli@vitesicure.it>

---
+++ src/main/kotlin/it/vitesicure/Application.kt
@@ -0,0 +1,17 @@
+package it.vitesicure
+
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import it.vitesicure.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
