a02139deb1e200b20397e877112815b148271d41
Gianni Bombelli <gbombelli@vitesicure.it>
2024-03-15 17:24:50 +0100

    create post stub method
    Signed-off-by: Gianni Bombelli <gbombelli@vitesicure.it>

--- src/main/kotlin/it/vitesicure/plugins/Routing.kt
+++ src/main/kotlin/it/vitesicure/plugins/Routing.kt
@@ -1,16 +1,14 @@
 package it.vitesicure.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respond(mapOf(
-                "Hello" to "World!",
-                "Good" to "evening"
-            ))
+        post("/users/{userId}/timeline") {
+            call.respond(HttpStatusCode.Created)
         }
     }
 }
